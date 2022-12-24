4e6d173cee287d3d218fecc7582bb596d9bc1c50
chen <23054115+cwkang1998@users.noreply.github.com>
2022-12-24 20:52:14 +0800

    feat: add simple db schema using prisma

---
+++ README.md
@@ -0,0 +1,9 @@
+# Tx Indexer
+
+A simple transaction indexer.
+
+## ERD
+
+A simple ERD showing the database schema.
+
+![ERDOverview](./prisma/ERD.svg)
---
+++ prisma/migrations/20221224074809_init/migration.sql
@@ -0,0 +1,61 @@
+-- CreateTable
+CREATE TABLE "TransactionType" (
+    "id" SMALLINT NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+
+    CONSTRAINT "TransactionType_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Receipt" (
+    "hash" VARCHAR(100) NOT NULL,
+    "payload" TEXT NOT NULL,
+
+    CONSTRAINT "Receipt_pkey" PRIMARY KEY ("hash")
+);
+
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" BIGINT NOT NULL,
+    "txHash" VARCHAR(100) NOT NULL,
+    "blockNumber" BIGINT NOT NULL,
+    "txId" BIGINT NOT NULL,
+    "from" VARCHAR(100) NOT NULL,
+    "to" VARCHAR(100) NOT NULL,
+    "txTypeId" INTEGER NOT NULL,
+
+    CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_ReceiptToTransaction" (
+    "A" VARCHAR(100) NOT NULL,
+    "B" BIGINT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "TransactionType_name_key" ON "TransactionType"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Transaction_txHash_key" ON "Transaction"("txHash");
+
+-- CreateIndex
+CREATE INDEX "Transaction_txHash_idx" ON "Transaction"("txHash");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Transaction_blockNumber_txId_key" ON "Transaction"("blockNumber", "txId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_ReceiptToTransaction_AB_unique" ON "_ReceiptToTransaction"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_ReceiptToTransaction_B_index" ON "_ReceiptToTransaction"("B");
+
+-- AddForeignKey
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_txTypeId_fkey" FOREIGN KEY ("txTypeId") REFERENCES "TransactionType"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ReceiptToTransaction" ADD CONSTRAINT "_ReceiptToTransaction_A_fkey" FOREIGN KEY ("A") REFERENCES "Receipt"("hash") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ReceiptToTransaction" ADD CONSTRAINT "_ReceiptToTransaction_B_fkey" FOREIGN KEY ("B") REFERENCES "Transaction"("id") ON DELETE CASCADE ON UPDATE CASCADE;
